a0c1e4f12071c98659a2e93c27f88129e7fcae53
Leandro Udala <leandro.correia@radixeng.com.br>
2023-05-27 04:45:26 -0300

    Removendo espaços no começo e fim da string, e substituindo dois espaços ou mais por um dentro da string

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,171 +1,182 @@
 <script lang="ts">
-    import "../styles.css";
-    import { onMount } from "svelte";
+	import '../styles.css';
+	import { onMount } from 'svelte';
 
-    interface Musica {
-        nome: String;
-        interprete: String;
-        codigo: Number;
-        trecho: String;
-        idioma: String;
-        index: String;
-    };
+	interface Musica {
+		nome: String;
+		interprete: String;
+		codigo: Number;
+		trecho: String;
+		idioma: String;
+		index: String;
+	}
 
-    let musicas: Musica[] = [];
-    let encontradas: Musica[] = []
+	let musicas: Musica[] = [];
+	let encontradas: Musica[] = [];
 
-    let search = "";
-    let hasText = false;
-    let searchTimeout = setTimeout(() => 0, 1);
-    let searching = false;
+	let search = '';
+	let hasText = false;
+	let searchTimeout = setTimeout(() => 0, 1);
+	let searching = false;
 
-    function removerAcentuacao(texto: string): string {
-        return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-    }
+	function removerAcentuacao(texto: string): string {
+		return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+	}
 
-    function limparString(texto: string): string {
-        return removerAcentuacao(texto).toLowerCase();
-    }
+	function corrigirEspacamento(str: string): string {
+		return str.replace(/\s{2,}/g, ' ');
+	}
 
-    function procurarMusica() {
-        const termo = limparString(search);
-        
-        encontradas = musicas.filter(musica =>  musica.index.indexOf(termo) != -1);
+	function limparString(texto: string): string {
+		return corrigirEspacamento(removerAcentuacao(texto).toLowerCase().trim());
+	}
 
-        searching = false;
-    }
+	function procurarMusica() {
+		const termo = limparString(search);
 
+		encontradas = musicas.filter((musica) => musica.index.indexOf(termo) != -1);
 
-    let isPlaying = false;
-    let backgroundMusic: HTMLAudioElement;
+		searching = false;
+	}
 
-    function tocarSom(codigo: Number) {
-        console.log(backgroundMusic);
-        const cod = (''+ codigo).padStart(5, '0');
-        const audioUrl = `https://ivideoke.com.br/static/mp3/${cod}.mp3`;
+	let isPlaying = false;
+	let backgroundMusic: HTMLAudioElement;
 
-        const currentUri = backgroundMusic.src
-        const sameAudio = currentUri == audioUrl;
+	function tocarSom(codigo: Number) {
+		console.log(backgroundMusic);
+		const cod = ('' + codigo).padStart(5, '0');
+		const audioUrl = `https://ivideoke.com.br/static/mp3/${cod}.mp3`;
 
-        if (isPlaying && sameAudio) {
-            backgroundMusic.pause();
-            isPlaying = false;
-            return;
-        }
+		const currentUri = backgroundMusic.src;
+		const sameAudio = currentUri == audioUrl;
 
-        if (!isPlaying && sameAudio) {
-            backgroundMusic.play();
-            isPlaying = true;
-            return;
-        }
+		if (isPlaying && sameAudio) {
+			backgroundMusic.pause();
+			isPlaying = false;
+			return;
+		}
 
-        backgroundMusic.src = audioUrl;
-        backgroundMusic.play();
-        isPlaying = true;
-    }
+		if (!isPlaying && sameAudio) {
+			backgroundMusic.play();
+			isPlaying = true;
+			return;
+		}
 
-    function onBuscaChanged() {
-        hasText = search.length != 0;
-        clearTimeout(searchTimeout);
-        encontradas = [];
-        
-        if (!hasText) {
-            return;
-        }
+		backgroundMusic.src = audioUrl;
+		backgroundMusic.play();
+		isPlaying = true;
+	}
 
-        searching = true;
-        searchTimeout = setTimeout(procurarMusica, 500);
-    }
+	function onBuscaChanged() {
+		hasText = search.length != 0;
+		clearTimeout(searchTimeout);
+		encontradas = [];
 
-    async function loadMusicas() {
-        const response  = await fetch("/songs_min.json");
-        if (!response.ok) {
-            alert("Não consegui baixar as músicas...");
-            return
-        }
+		if (!hasText) {
+			return;
+		}
 
-        const rawMusicas: Musica[] = await response.json()
-        musicas = rawMusicas.map(musica => {
-            musica.index = limparString(
-                (musica.interprete + '_') +
-                (musica.nome + '_') + 
-                (musica.trecho + '')
-            );
+		searching = true;
+		searchTimeout = setTimeout(procurarMusica, 500);
+	}
 
-            return musica;
-        });
-        console.log("Músicas encontradas:", musicas.length);
-    }
+	async function loadMusicas() {
+		const response = await fetch('/songs_min.json');
+		if (!response.ok) {
+			alert('Não consegui baixar as músicas...');
+			return;
+		}
 
-    onMount(() => {
-        loadMusicas();
-    });
+		const rawMusicas: Musica[] = await response.json();
+		musicas = rawMusicas.map((musica) => {
+			musica.index = limparString(
+				(musica.interprete + '_').trim() + (musica.nome + '_').trim() + (musica.trecho + '').trim()
+			);
+
+			return musica;
+		});
+		console.log('Músicas encontradas:', musicas.length);
+	}
+
+	onMount(() => {
+		loadMusicas();
+	});
 </script>
+
 <svelte:head>
-    <title>Karaokê do Sandro</title>
+	<title>Karaokê do Sandro</title>
 </svelte:head>
 
 <main>
-    <audio src="" bind:this={backgroundMusic}></audio>
-    <div class:flex-container={!hasText}>
-        <div class="header">
-            <div class="container center bg-white" class:logo={!hasText}>
-                <img src="/favicon.png" class:image-small="{hasText}" alt="Videokê Logo - Leão cantando e segurando o microfone com a mão direita."/>
-            </div>
-            {#if hasText}
-            <div class="container center">
-                <div class="title">
-                    Karaokê<br>
-                    do<br>
-                    Sandro
-                </div>
-                <div>
-                    {musicas.length} músicas
-                </div>
-            </div>
-            {/if}
-        </div>
-        <div class="container search-container mb-4">
-            <div class="p-2">
-                <input type="search" bind:value={search} on:input={onBuscaChanged} class="form-control w-100" placeholder="Buscar por Músicas, Intérpretes">
-            </div>
-        </div>
-    </div>
-    <div class="container container-fill" class:hidden="{!hasText}">
-        <div class="scrollable-content">
-                <table class="table table-striped">
-                    <thead class="thead-green">
-                        <tr>
-                            <th scope="col">Código</th>
-                            <th scope="col">Intérprete</th>
-                            <th scope="col">Título</th>
-                            <th scope="col">Início da Letra</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {#if encontradas.length }
-                        {#each encontradas as musica}
-                        <tr on:click={() => tocarSom(musica.codigo) }>
-                            <td>{musica.codigo}</td>
-                            <td>{musica.interprete}</td>
-                            <td>{musica.nome}</td>
-                            <td>{musica.trecho}</td>
-                        </tr>
-                        {/each}
-                        {:else}
-                            <tr>
-                                <td class="center" colspan="4">
-                                    {#if searching}
-                                    Procurando...
-                                    {:else}
-                                    Nenhuma música encontrada.
-                                    {/if}
-                                </td>
-                            </tr>
-                        {/if}
-                    </tbody>
-                </table>
-            </div>
-    </div>
+	<audio src="" bind:this={backgroundMusic} />
+	<div class:flex-container={!hasText}>
+		<div class="header">
+			<div class="container center bg-white" class:logo={!hasText}>
+				<img
+					src="/favicon.png"
+					class:image-small={hasText}
+					alt="Videokê Logo - Leão cantando e segurando o microfone com a mão direita."
+				/>
+			</div>
+			{#if hasText}
+				<div class="container center">
+					<div class="title">
+						Karaokê<br />
+						do<br />
+						Sandro
+					</div>
+					<div>
+						{musicas.length} músicas
+					</div>
+				</div>
+			{/if}
+		</div>
+		<div class="container search-container mb-4">
+			<div class="p-2">
+				<input
+					type="search"
+					bind:value={search}
+					on:input={onBuscaChanged}
+					class="form-control w-100"
+					placeholder="Buscar por Músicas, Intérpretes"
+				/>
+			</div>
+		</div>
+	</div>
+	<div class="container container-fill" class:hidden={!hasText}>
+		<div class="scrollable-content">
+			<table class="table table-striped">
+				<thead class="thead-green">
+					<tr>
+						<th scope="col">Código</th>
+						<th scope="col">Intérprete</th>
+						<th scope="col">Título</th>
+						<th scope="col">Início da Letra</th>
+					</tr>
+				</thead>
+				<tbody>
+					{#if encontradas.length}
+						{#each encontradas as musica}
+							<tr on:click={() => tocarSom(musica.codigo)}>
+								<td>{musica.codigo}</td>
+								<td>{musica.interprete}</td>
+								<td>{musica.nome}</td>
+								<td>{musica.trecho}</td>
+							</tr>
+						{/each}
+					{:else}
+						<tr>
+							<td class="center" colspan="4">
+								{#if searching}
+									Procurando...
+								{:else}
+									Nenhuma música encontrada.
+								{/if}
+							</td>
+						</tr>
+					{/if}
+				</tbody>
+			</table>
+		</div>
+	</div>
 </main>
-
